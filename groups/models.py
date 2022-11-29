4bf2fac5bb76c3a02b68d2e9a01a5e92aa880fd4
HigorSkw <higor.skowronski@gmail.com>
2022-11-29 01:26:26 -0300

    feat: create apps

--- groups/models.py
+++ groups/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 
 class Group(models.Model):
-    ...
+    scientific_name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateField(auto_now=True)
+
+    pet = models.ForeignKey(
+        "pets.Pet",
+        on_delete=models.CASCADE,
+        related_name="groups",
+    )
+
+
+# N para 1 com pet
--- pets/models.py
+++ pets/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
-# Create your models here.
-# class Pet
+
+class SexPet(models.TextChoices):
+    MALE = "Male"
+    FEMALE = "Female"
+    NOT_INFORMED = "Not_Informed"
+
+
 class Pet(models.Model):
-    ...
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=20,
+        choices=SexPet.choices,
+        default=SexPet.NOT_INFORMED,
+    )
